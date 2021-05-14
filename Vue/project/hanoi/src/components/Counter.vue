eb501e81ac25d1a489330f7acb008a8a52284e0f
Drasty4kLink <you@example.com>
2021-05-14 20:49:09 +0300

    lecture 5 adv

---
+++ Vue/project/hanoi/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input type="button" :value="clicks" @click="increment">
+        <input type="button" value="reset" @click="reset">
+    </div>
+  
+
+</template>
+
+<script>
+
+export default{
+    name: 'counter',
+    data() {
+        return {
+            clicks: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.clicks++
+        },
+        reset() {
+            this.clicks = 0
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
